eb401a9c98519b92835e20915fcfafa419a614b7
Robert Hensing <robert@roberthensing.nl>
2020-03-04 00:53:53 +0100

    nix-install.sh: Add locking

--- deploy_nixos/nix-install.sh
+++ deploy_nixos/nix-install.sh
@@ -8,15 +8,20 @@ if ! type "nix-instantiate" > /dev/null; then
   export NIX_STATE_DIR=~/.my-store/var
 fi
 
-if ! type "nix-instantiate" > /dev/null; then
-  echo 1>&2 "WARNING: nix-instantiate not found."
-  echo 1>&2 "NOTE: Fetching static nix."
-  tarball_url="https://gist.github.com/roberth/422f3bab3ed8e9c0af5790bda0ce37cd/raw/8601326c6ea35056212a9944fe82cb04dd0fc8ad/nix.tar"
+(
+  exec 100>/tmp/nix-install.lock || exit 1
+  flock 100 || exit 1
 
-  store="$HOME/.my-store"
-  nix_conf="$NIX_CONF_DIR/nix.conf"
-  mkdir -p ~/.static-nix
-  curl -L "$tarball_url" | tar -xC ~/.static-nix
-  mkdir -p "$NIX_DATA_DIR" "$store" "$NIX_CONF_DIR" "$NIX_LOG_DIR" "$NIX_STATE_DIR"
-  echo "store = $store" >"$nix_conf"
-fi
+  if ! type "nix-instantiate" > /dev/null; then
+    echo 1>&2 "WARNING: nix-instantiate not found."
+    echo 1>&2 "NOTE: Fetching static nix."
+    tarball_url="https://gist.github.com/roberth/422f3bab3ed8e9c0af5790bda0ce37cd/raw/8601326c6ea35056212a9944fe82cb04dd0fc8ad/nix.tar"
+
+    store="$HOME/.my-store"
+    nix_conf="$NIX_CONF_DIR/nix.conf"
+    mkdir -p ~/.static-nix
+    curl -L "$tarball_url" | tar -xC ~/.static-nix
+    mkdir -p "$NIX_DATA_DIR" "$store" "$NIX_CONF_DIR" "$NIX_LOG_DIR" "$NIX_STATE_DIR"
+    echo "store = $store" >"$nix_conf"
+  fi
+)
